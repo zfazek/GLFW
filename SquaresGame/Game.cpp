3e14264541a2b9cc450173839decb46c8d5332f9
Zoltan Fazekas <ongroa@gmail.com>
2017-04-11 22:54:41 +0200

    fix OSX compatibility

--- SquaresGame/Game.cpp
+++ SquaresGame/Game.cpp
@@ -19,11 +19,16 @@ Game::~Game() {
 }
 
 void Game::init() {
+#ifdef __APPLE__
+    ResourceManager::loadTexture("/Users/zfazek/git/GLFW/SquaresGame/resources/block.png", GL_TRUE, "face");
+    ResourceManager::loadShader("/Users/zfazek/git/GLFW/SquaresGame/shaders/vertex.glsl", "/Users/zfazek/git/GLFW/SquaresGame/shaders/fragment.glsl", nullptr, "sprite");
+#else
+    ResourceManager::loadTexture("resources/block.png", GL_TRUE, "face");
     ResourceManager::loadShader("shaders/vertex.glsl", "shaders/fragment.glsl", nullptr, "sprite");
+#endif
     glm::mat4 projection = glm::ortho(0.0f, static_cast<GLfloat>(this->width), static_cast<GLfloat>(this->height), 0.0f, -1.0f, 1.0f);
     ResourceManager::getShader("sprite").use().setInteger("image", 0);
     ResourceManager::getShader("sprite").setMatrix4("projection", projection);
-    ResourceManager::loadTexture("resources/block.png", GL_TRUE, "face");
     renderer = new SpriteRenderer(ResourceManager::getShader("sprite"));
 }
 
